d703d09af8aa26838aa1f25bed55e83f0068d532
Christian Shinkle <cshinkle2018@gmail.com>
2022-04-19 21:27:37 -0500

    lint, pull rng out

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,28 @@
 pub mod pass_gen {
-    use rand::{Rng, thread_rng};
     use rand::rngs::ThreadRng;
+    use rand::Rng;
 
     const NUMBERS: [char; 10] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    const LOWER: [char; 26] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    const UPPER: [char; 26] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-    const SPECIALS: [char; 32] = ['!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
-    const ALL: [char; 94] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
+    const LOWER: [char; 26] = [
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+        's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    ];
+    const UPPER: [char; 26] = [
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+        'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    ];
+    const SPECIALS: [char; 32] = [
+        '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<',
+        '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~',
+    ];
+    const ALL: [char; 94] = [
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+        'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*',
+        '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`',
+        '{', '|', '}', '~',
+    ];
 
     fn sample(rng: &mut ThreadRng, data: &[char]) -> u8 {
         data[rng.gen_range(0..data.len())] as u8
@@ -20,21 +36,20 @@ pub mod pass_gen {
     }
 
     //build vec of random chars, then build String from vec
-    pub fn pass_gen(size: usize) -> String {
-        //setup randomness and password
-        let mut rng = thread_rng();
+    pub fn pass_gen(size: usize, rng: &mut ThreadRng) -> String {
+        //setup password
         let mut password: Vec<u8> = Vec::with_capacity(size);
         //guarantee at least one of each type of letter
-        password.push(sample(&mut rng, &NUMBERS));
-        password.push(sample(&mut rng, &LOWER));
-        password.push(sample(&mut rng, &UPPER));
-        password.push(sample(&mut rng, &SPECIALS));
+        password.push(sample(rng, &NUMBERS));
+        password.push(sample(rng, &LOWER));
+        password.push(sample(rng, &UPPER));
+        password.push(sample(rng, &SPECIALS));
         //randomly sample the rest
         for _ in 4..size {
-            password.push(sample(&mut rng, &ALL));
+            password.push(sample(rng, &ALL));
         }
         //shuffle to guarantee random order
-        shuffle(&mut rng, &mut password);
+        shuffle(rng, &mut password);
         //build (unsafe) ASCII String quickly
         return unsafe { String::from_utf8_unchecked(password) };
     }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use pass_gen::pass_gen::pass_gen;
+use rand::thread_rng;
 
 fn main() {
-    let password = pass_gen(15);
-
-    println!("{:?}", password);
+    let mut rng = thread_rng();
+    for _ in 0..1 {
+        println!("{}", pass_gen(10, &mut rng));
+    }
 }
