695cdb305f53cdadfe0d33d9a565df3445904ca9
Christian Shinkle <cshinkle2018@gmail.com>
2022-07-07 22:03:29 -0500

    constrained size to u32 from usize, reduced runtime by 3%

--- src/lib.rs
+++ src/lib.rs
@@ -5,22 +5,21 @@ use rand::prelude::{Rng, SliceRandom, ThreadRng};
 // UPPER: 36..62;
 // SPECIALS: 62..94;
 const LETTERS: [char; 94] = [
-    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-    'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-    'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*',
-    '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`',
-    '{', '|', '}', '~',
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+    'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B',
+    'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+    'V', 'W', 'X', 'Y', 'Z', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.',
+    '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~',
 ];
 
-pub fn pass_gen(size: usize, rng: &mut ThreadRng) -> String {
-    let non_random_indices: Vec<usize> = (0..size).collect();
-    let random_indices: Vec<&usize> = non_random_indices.choose_multiple(rng, 4).collect();
-    let mut password: Vec<u8> = vec![0; size];
-    password[*random_indices[0]] = sample(rng, 10, 00);
-    password[*random_indices[1]] = sample(rng, 26, 10);
-    password[*random_indices[2]] = sample(rng, 26, 36);
-    password[*random_indices[3]] = sample(rng, 32, 62);
+pub fn pass_gen(size: u32, rng: &mut ThreadRng) -> String {
+    let non_random_indices = Vec::from_iter(0..size);
+    let random_indices: Vec<&u32> = non_random_indices.choose_multiple(rng, 4).collect();
+    let mut password: Vec<u8> = vec![0; size as usize];
+    password[*random_indices[0] as usize] = sample(rng, 10, 00);
+    password[*random_indices[1] as usize] = sample(rng, 26, 10);
+    password[*random_indices[2] as usize] = sample(rng, 26, 36);
+    password[*random_indices[3] as usize] = sample(rng, 32, 62);
     for letter in password.iter_mut() {
         if *letter == 0 {
             *letter = sample(rng, 94, 0);
