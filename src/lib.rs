fb312e2c4e4c0e51349a3b367ae052f6c7b5f404
Christian Shinkle <cshinkle2018@gmail.com>
2022-07-10 15:41:45 -0500

    renamed indices to indexes

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
-use rand::prelude::{Rng, SliceRandom, ThreadRng};
+use rand::rngs::ThreadRng;
+use rand::seq::SliceRandom;
+use rand::Rng;
+use std::iter::FromIterator;
 
 // NUMBERS: 0..10;
 // LOWER: 10..36;
@@ -13,13 +16,13 @@ const LETTERS: [char; 94] = [
 ];
 
 pub fn pass_gen(size: u32, rng: &mut ThreadRng) -> String {
-    let non_random_indices = Vec::from_iter(0..size);
-    let random_indices: Vec<&u32> = non_random_indices.choose_multiple(rng, 4).collect();
+    let non_random_indexes = Vec::from_iter(0..size);
+    let random_indexes: Vec<&u32> = non_random_indexes.choose_multiple(rng, 4).collect();
     let mut password: Vec<u8> = vec![0; size as usize];
-    password[*random_indices[0] as usize] = sample(rng, 10, 00);
-    password[*random_indices[1] as usize] = sample(rng, 26, 10);
-    password[*random_indices[2] as usize] = sample(rng, 26, 36);
-    password[*random_indices[3] as usize] = sample(rng, 32, 62);
+    password[*random_indexes[0] as usize] = sample(rng, 10, 00);
+    password[*random_indexes[1] as usize] = sample(rng, 26, 10);
+    password[*random_indexes[2] as usize] = sample(rng, 26, 36);
+    password[*random_indexes[3] as usize] = sample(rng, 32, 62);
     for letter in password.iter_mut() {
         if *letter == 0 {
             *letter = sample(rng, 94, 0);
