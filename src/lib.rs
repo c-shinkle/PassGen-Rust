47938cd8aa851e6ef168b8119ac41f5249930130
Christian Shinkle <cshinkle2018@gmail.com>
2022-10-30 22:58:50 -0500

    fixed types, lint

--- src/lib.rs
+++ src/lib.rs
@@ -4,26 +4,26 @@ use rand::{Rng, RngCore};
 // LOWER: 10..36;
 // UPPER: 36..62;
 // SPECIALS: 62..94;
-const LETTERS: &[u8] = b"0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+const CHARS: &[u8] = b"0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 
 pub fn pass_gen(buffer: &mut [u8], rng: &mut impl RngCore) {
     assert!(buffer.len() as u32 >= 4);
     for letter in buffer.iter_mut() {
         *letter = sample(rng, 94, 0);
     }
-    let random_indexes = get_random_indexes(buffer.len() as u32, rng);
-    buffer[random_indexes[0] as usize] = sample(rng, 10, 00);
-    buffer[random_indexes[1] as usize] = sample(rng, 26, 10);
-    buffer[random_indexes[2] as usize] = sample(rng, 26, 36);
-    buffer[random_indexes[3] as usize] = sample(rng, 32, 62);
+    let random_indexes = get_random_indexes(buffer.len(), rng);
+    buffer[random_indexes[0]] = sample(rng, 10, 00);
+    buffer[random_indexes[1]] = sample(rng, 26, 10);
+    buffer[random_indexes[2]] = sample(rng, 26, 36);
+    buffer[random_indexes[3]] = sample(rng, 32, 62);
 }
 
 #[inline(always)]
 fn sample(rng: &mut impl RngCore, length: u32, offset: u32) -> u8 {
-    LETTERS[((rng.gen::<u32>() % length) + offset) as usize]
+    CHARS[((rng.gen::<u32>() % length) + offset) as usize]
 }
 
-fn get_random_indexes(len: u32, rng: &mut impl RngCore) -> [u32; 4] {
+fn get_random_indexes(len: usize, rng: &mut impl RngCore) -> [usize; 4] {
     assert!(len >= 4);
     let mut indexes = Vec::with_capacity(4);
     for j in len - 4..len {
@@ -39,8 +39,8 @@ fn get_random_indexes(len: u32, rng: &mut impl RngCore) -> [u32; 4] {
 
 #[cfg(test)]
 mod tests {
-    use rand::prelude::*;
     use crate::get_random_indexes;
+    use rand::prelude::*;
 
     #[test]
     fn should_insert_with_duplicates() {
