afc4612f2e8c3c5e97afa591bcf83840da6f02f3
Christian Shinkle <cshinkle2018@gmail.com>
2022-11-28 23:13:51 -0600

    inlined random_indices function, removed cast

--- src/lib.rs
+++ src/lib.rs
@@ -12,42 +12,22 @@ pub fn pass_gen(buffer: &mut [u8], rng: &mut impl RngCore) {
     for letter in buffer.iter_mut() {
         *letter = sample(rng, 94, 0);
     }
-    let random_indexes = get_random_indexes(len, rng);
-    buffer[random_indexes[0]] = sample(rng, 10, 00);
-    buffer[random_indexes[1]] = sample(rng, 26, 10);
-    buffer[random_indexes[2]] = sample(rng, 26, 36);
-    buffer[random_indexes[3]] = sample(rng, 32, 62);
+    let mut random_indices = Vec::with_capacity(4);
+    for j in len - 4..len {
+        let random_index = rng.gen_range(0..=j);
+        if let Some(index) = random_indices.iter().position(|index| *index == random_index) {
+            random_indices.insert(index, j);
+        } else {
+            random_indices.push(random_index);
+        }
+    }
+    buffer[random_indices[0]] = sample(rng, 10, 00);
+    buffer[random_indices[1]] = sample(rng, 26, 10);
+    buffer[random_indices[2]] = sample(rng, 26, 36);
+    buffer[random_indices[3]] = sample(rng, 32, 62);
 }
 
 #[inline(always)]
 fn sample(rng: &mut impl RngCore, length: usize, offset: usize) -> u8 {
-    CHARS[((rng.gen::<usize>() % length) + offset) as usize]
+    CHARS[((rng.gen::<usize>() % length) + offset)]
 }
-
-#[inline(always)]
-fn get_random_indexes(len: usize, rng: &mut impl RngCore) -> Vec<usize> {
-    assert!(len >= 4);
-    let mut indexes = Vec::with_capacity(4);
-    for j in len - 4..len {
-        let random_index = rng.gen_range(0..=j);
-        if let Some(index) = indexes.iter().position(|index| *index == random_index) {
-            indexes.insert(index, j);
-        } else {
-            indexes.push(random_index);
-        }
-    }
-    indexes
-}
-
-// #[cfg(test)]
-// mod tests {
-//     use crate::get_random_indexes;
-//     use rand::prelude::*;
-//
-//     #[test]
-//     fn should_insert_with_duplicates() {
-//         let mut rng = StdRng::seed_from_u64(0);
-//         let actual = get_random_indexes(4, &mut rng);
-//         assert_eq!(actual, [0, 3, 1, 2]);
-//     }
-// }
